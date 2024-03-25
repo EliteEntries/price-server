266877c67061601127080c33dbebfd1b7c662ad0
George Abreu Jr. <george8794@gmail.com>
2024-03-25 00:58:23 +0000

    alpaca price server rebuilt (no ws)

--- src/exchanges/alpaca.js
+++ src/exchanges/alpaca.js
@@ -12,203 +12,118 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getCryptoPrices = exports.getStockPrices = exports.getOptionsPrices = void 0;
 const alpaca_trade_api_1 = __importDefault(require("@alpacahq/alpaca-trade-api"));
 const redis_1 = require("redis");
-const ws_1 = __importDefault(require("ws"));
+const https_1 = __importDefault(require("https"));
 require("dotenv/config");
 const Publisher = (0, redis_1.createClient)({
     url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_URL}`
 });
 Publisher.on('error', (err) => console.log('Redis Publisher Error', err));
-let candleTimers = {};
-let tradeTimers = {};
-let lastTrades = {};
-let Subscriptions = [];
 const alpaca = new alpaca_trade_api_1.default({
     keyId: process.env.ALPACA_KEY,
     secretKey: process.env.ALPACA_SECRET,
     paper: true,
 });
-let server;
-function Socket() {
+let Stocks = [], Cryptos = [], Options = [], Candles = [];
+function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield Publisher.connect();
-        const result = yield Publisher.get('exchanges:alpaca:tracked');
-        server = new ws_1.default('wss://stream.data.alpaca.markets/v2/iex');
-        candleTimers = {}, tradeTimers = {};
-        server.addEventListener('error', function (e) {
-            return __awaiter(this, void 0, void 0, function* () {
-                yield Publisher.set('exchanges:alpaca:tracked', JSON.stringify(Subscriptions));
-                server.close();
-                setTimeout(Socket, 5000);
-                process.send && process.send(['alpaca', 'error']);
-            });
-        });
-        server.addEventListener('close', function (e) {
-            return __awaiter(this, void 0, void 0, function* () {
-                yield Publisher.set('exchanges:alpaca:tracked', JSON.stringify(Subscriptions));
-                server.close();
-                setTimeout(Socket, 5000);
-                process.send && process.send(['alpaca', 'lost-connection']);
-            });
-        });
-        server.addEventListener('open', function (e) {
-            return __awaiter(this, void 0, void 0, function* () {
-                process.send && process.send(['alpaca', 'open']);
-                server.on('message', (resp) => __awaiter(this, void 0, void 0, function* () {
-                    const msgs = JSON.parse(resp);
-                    msgs.forEach((msg) => __awaiter(this, void 0, void 0, function* () {
-                        const message = msg.msg;
-                        const status = msg.T;
-                        if (status == 'success' && message)
-                            switch (message) {
-                                case 'connected': {
-                                    server.send(JSON.stringify({
-                                        action: 'auth',
-                                        key: process.env.ALPACA_KEY,
-                                        secret: process.env.ALPACA_SECRET
-                                    }));
-                                    break;
-                                }
-                                case 'authenticated': {
-                                    if (result) {
-                                        let trades = [], candles = [];
-                                        JSON.parse(result).forEach((subscription) => {
-                                            subscription[0] == 'sub-trades' && trades.push(subscription[1]);
-                                            subscription[0] == 'sub-candles' && candles.push(subscription[1]);
-                                        });
-                                        server.send(JSON.stringify({
-                                            action: 'subscribe',
-                                            trades: trades,
-                                            bars: candles
-                                        }));
-                                    }
-                                    break;
-                                }
-                                default: {
-                                    console.log('----', message);
-                                    break;
-                                }
-                            }
-                        else
-                            switch (status) {
-                                case 't': {
-                                    yield Publisher.set('price:alpaca:' + msg.S, msg.p);
-                                    console.log('');
-                                    break;
-                                }
-                                case 'subscription': {
-                                    msg.trades.length > 0 && console.log(`Subscribed to ${msg.trades} trades`);
-                                    msg.bars.length > 0 && console.log(`Subscribed to ${msg.bars} candles`);
-                                    break;
-                                }
-                                default: {
-                                    console.log(msg);
-                                    break;
-                                }
-                            }
-                    }));
-                }));
-            });
-        });
-    });
-}
-function subscribeCandles(market, timeframe, first) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            // let candles = await getCandles(market, timeframe, first ? 200 : 1)
-            console.log('candles');
-        }
-        catch (e) {
-            setTimeout(() => subscribeCandles(market, timeframe, false), 1000);
-        }
-    });
-}
-function executeMessage(message) {
-    return __awaiter(this, void 0, void 0, function* () {
-        switch (message[0]) {
-            case 'sub-trades': {
-                if (!Subscriptions.toString().includes(message.toString())) {
-                    server === null || server === void 0 ? void 0 : server.send(JSON.stringify({
-                        action: 'subscribe',
-                        trades: [message[1].toUpperCase()]
-                    }));
-                    process.send && process.send(['alpaca', 'sub-trades', message[1].toUpperCase()]);
-                    Subscriptions.push(message);
-                    Publisher.set('exchanges:alpaca:tracked', JSON.stringify(Subscriptions));
-                }
-                break;
-            }
-            case 'unsub-trades': {
-                if (Subscriptions.filter((sub) => { return (sub[0] == 'sub-trades' && sub[1] == message[1]); }).length == 1) {
-                    server === null || server === void 0 ? void 0 : server.send(JSON.stringify({
-                        action: 'unsubscribe',
-                        trades: [message[1].toUpperCase()],
-                    }));
-                    Publisher.del(`${message[1].toUpperCase()}-alpaca`);
-                    Subscriptions.forEach((sub, index) => {
-                        if (sub[0] == 'sub-trades' && sub[1] == message[1]) {
-                            Subscriptions.splice(index, 1);
-                            Publisher.set('exchanges:alpaca:tracked', JSON.stringify(Subscriptions));
-                        }
-                    });
-                }
-                break;
-            }
-        }
-    });
-}
-process.on('message', (message) => {
-    executeMessage(message);
-});
-const equities = alpaca.data_stream_v2;
-const crypto = alpaca.crypto_stream_v2;
-const news = alpaca.news_stream;
-crypto.onConnect(() => __awaiter(void 0, void 0, void 0, function* () {
-    console.log('crypto connected');
-}));
-crypto.onCryptoTrade((trade) => {
-    console.log('Crypto Trade:', trade);
-});
-crypto.onError((err) => {
-    console.log("Crypto Error:", err);
-});
-equities.onConnect(() => __awaiter(void 0, void 0, void 0, function* () {
-    process.send && process.send(['alpaca', 'open']);
-    console.log('open');
-}));
-equities.onStateChange((status) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log("Status:", status);
-    switch (status) {
-        case 'authenticated': {
+        if (!Publisher.isOpen)
             yield Publisher.connect();
-            const result = yield Publisher.get('exchanges:alpaca:tracked');
-            if (result) {
-                let trades = [], candles = [];
-                JSON.parse(result).forEach((subscription) => {
-                    subscription[0] == 'sub-trades' && trades.push(subscription[1]);
-                    subscription[0] == 'sub-candles' && candles.push(subscription[1]);
-                });
-                equities.subscribeForTrades(trades);
-                equities.subscribeForBars(candles);
-            }
-            break;
+        const result = yield Publisher.get('exchanges:alpaca:tracked');
+        if (result) {
+            JSON.parse(result).forEach((subscription) => {
+                subscription[0] == 'stock-trades' && Stocks.push(subscription[1]);
+                subscription[0] == 'crypto-trades' && Cryptos.push(subscription[1]);
+                subscription[0] == 'option-prices' && Options.push(subscription[1]);
+                subscription[0] == 'sub-candles' && Candles.push(subscription[1]);
+            });
         }
-    }
-}));
-equities.onError((err) => {
-    console.log("Error:", err);
-});
-equities.onStockTrade((trade) => {
-    console.log(`${trade.Size} shares ${trade.Symbol} @ ${trade.Price}`);
-});
-equities.onStockBar((bar) => {
-    console.log("Bar:", bar);
-});
-equities.onStatuses((statuses) => {
-    console.log("Statuses:", statuses);
-});
-equities.addListener('message', (msg) => {
-    console.log(msg);
-});
-equities.connect();
+        setInterval(() => __awaiter(this, void 0, void 0, function* () {
+            if (Stocks.length && Stocks.length > 0)
+                yield getStockPrices(Stocks);
+            if (Cryptos.length && Cryptos.length > 0)
+                yield getCryptoPrices(Cryptos);
+            if (Options.length && Options.length > 0)
+                yield getOptionsPrices(Options);
+        }), 1000);
+    });
+}
+main();
+function getOptionsPrices(data) {
+    const symbols = Array.isArray(data) ? data.join(',') : data;
+    const opts = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            'APCA-API-KEY-ID': process.env.ALPACA_KEY,
+            'APCA-API-SECRET-KEY': process.env.ALPACA_SECRET
+        }
+    };
+    https_1.default.get(`https://data.alpaca.markets/v1beta1/options/quotes/latest?symbols=${symbols}&feed=indicative`, opts, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            data = JSON.parse(data);
+            Object.keys(data.quotes).forEach((symbol) => __awaiter(this, void 0, void 0, function* () {
+                const bid = data.quotes[symbol].bp;
+                const ask = data.quotes[symbol].ap;
+                const bidSize = data.quotes[symbol].bs;
+                const askSize = data.quotes[symbol].as;
+                yield Publisher.set('bid:alpaca:' + symbol, bid);
+                yield Publisher.set('ask:alpaca:' + symbol, ask);
+            }));
+        });
+        res.on('error', (err) => {
+            console.log(err);
+        });
+    });
+}
+exports.getOptionsPrices = getOptionsPrices;
+function getStockPrices(symbols) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!Array.isArray(symbols))
+            symbols = [symbols];
+        const resp = yield alpaca.getLatestTrades(symbols);
+        resp.forEach((trade) => {
+            Publisher.set('price:alpaca:' + trade.Symbol, trade.Price);
+        });
+    });
+}
+exports.getStockPrices = getStockPrices;
+function getCryptoPrices(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const symbols = Array.isArray(data) ? data.join(',') : data;
+        symbols.replace('/', '%2F');
+        const opts = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                'APCA-API-KEY-ID': process.env.ALPACA_KEY,
+                'APCA-API-SECRET-KEY': process.env.ALPACA_SECRET
+            }
+        };
+        https_1.default.get(`https://data.alpaca.markets/v1beta3/crypto/us/latest/trades?symbols=${symbols}`, opts, (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                data = JSON.parse(data);
+                Object.keys(data.trades).forEach((symbol) => {
+                    const trade = data.trades[symbol];
+                    if (Date.now() - (new Date(trade.t)).getTime() > (60000 * 60))
+                        return;
+                    Publisher.set('price:alpaca:' + symbol, trade.p);
+                });
+            });
+            res.on('error', (err) => {
+                console.log(err);
+            });
+        });
+    });
+}
+exports.getCryptoPrices = getCryptoPrices;
--- src/exchanges/alpaca.ts
+++ src/exchanges/alpaca.ts
@@ -1,6 +1,6 @@
 import Alpaca from '@alpacahq/alpaca-trade-api'
 import redis, { RedisClientType, createClient } from 'redis'
-import WebSocket from 'ws'
+import https from 'https'
 import 'dotenv/config'
 
 const Publisher: RedisClientType = createClient({
@@ -8,202 +8,106 @@ const Publisher: RedisClientType = createClient({
 })
 Publisher.on('error', (err) => console.log('Redis Publisher Error', err))
 
-let candleTimers: any = {}
-let tradeTimers: any = {}
-let lastTrades: any = {}
-let Subscriptions: any[] = []
 
 const alpaca = new Alpaca({
     keyId: process.env.ALPACA_KEY,
     secretKey: process.env.ALPACA_SECRET,
     paper: true,
 })
-
-let server: WebSocket
-
-async function Socket() {
-    await Publisher.connect()
+let Stocks: any[] = [], Cryptos:any[] = [], Options: any[] = [],
+    Candles: any[] = []
+async function main() {
+    if (!Publisher.isOpen) await Publisher.connect();
     const result = await Publisher.get('exchanges:alpaca:tracked')
-    server = new WebSocket('wss://stream.data.alpaca.markets/v2/iex')
+    if (result) {
+        JSON.parse(result).forEach( (subscription: string[]) => { 
+            subscription[0] == 'stock-trades' && Stocks.push(subscription[1])
+            subscription[0] == 'crypto-trades' && Cryptos.push(subscription[1])
+            subscription[0] == 'option-prices' && Options.push(subscription[1])
+            subscription[0] == 'sub-candles' && Candles.push(subscription[1])
+        })
 
-    candleTimers = {}, tradeTimers = {}
+    }
+
+    setInterval( async ()=>{
+        if (Stocks.length && Stocks.length > 0) await getStockPrices(Stocks)
+        if (Cryptos.length && Cryptos.length > 0) await getCryptoPrices(Cryptos)
+        if (Options.length && Options.length > 0) await getOptionsPrices(Options)
+    }, 1000)
+
+}
+main()
+
+export function getOptionsPrices(data: string|string[]) {
+    const symbols = Array.isArray(data) ? data.join(',') : data
+    const opts = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          'APCA-API-KEY-ID': process.env.ALPACA_KEY,
+          'APCA-API-SECRET-KEY': process.env.ALPACA_SECRET
+        }
+      };
     
-    server.addEventListener('error', async function(e) {
-        await Publisher.set('exchanges:alpaca:tracked', JSON.stringify(Subscriptions))
-        server.close()
-        setTimeout(Socket, 5000)
-        process.send && process.send(['alpaca', 'error'])
-    })
-
-    server.addEventListener('close', async function(e) {
-        await Publisher.set('exchanges:alpaca:tracked', JSON.stringify(Subscriptions))
-        server.close()
-        setTimeout(Socket, 5000)
-        process.send && process.send(['alpaca', 'lost-connection'])
-    })
-
-    server.addEventListener('open', async function(e) {
-        process.send && process.send(['alpaca', 'open'])
-        server.on('message', async (resp: string) => {
-            const msgs: string[] = JSON.parse(resp)
-            msgs.forEach( async (msg: any) => {
-                const message = msg.msg
-                const status = msg.T
-
-                if (status == 'success' && message) 
-                    switch (message) {
-                        case 'connected': {
-                            server.send(JSON.stringify({
-                                action : 'auth',
-                                key : process.env.ALPACA_KEY,
-                                secret : process.env.ALPACA_SECRET
-                            }))
-                            break;
-                        }
-                        case 'authenticated' : {            
-                            if (result) {
-                                let trades: string[] = [], candles: string[] = []
-                                JSON.parse(result).forEach( (subscription: string[]) =>{ 
-                                    subscription[0] == 'sub-trades' && trades.push(subscription[1])
-                                    subscription[0] == 'sub-candles' && candles.push(subscription[1])
-                                })
-                                server.send(JSON.stringify({
-                                    action : 'subscribe',
-                                    trades : trades,
-                                    bars : candles
-                                }))
-                            }
-                            break;
-                        }
-                        default : {
-                            console.log('----', message)
-                            break;
-                        }
-                    }
-                else switch (status) {
-                    case 't' : {
-                        await Publisher.set('price:alpaca:'+msg.S, msg.p)
-                        console.log('')
-                        break;
-                    }
-                    case 'subscription' : {
-                        msg.trades.length > 0 && console.log(`Subscribed to ${msg.trades} trades`)
-                        msg.bars.length > 0 && console.log(`Subscribed to ${msg.bars} candles`)
-                        break;
-                    }
-                    default : {
-                        console.log(msg)
-                        break;
-                    }
-                }
+    https.get(`https://data.alpaca.markets/v1beta1/options/quotes/latest?symbols=${symbols}&feed=indicative`, opts,
+        (res) => {
+            let data: any = ''
+            res.on('data', (chunk) => {
+                data += chunk
+            })
+            res.on('end', () => {
+                data = JSON.parse(data)
+                Object.keys(data.quotes).forEach(async (symbol: string) => {
+                    const bid = data.quotes[symbol].bp
+                    const ask = data.quotes[symbol].ap
+                    const bidSize = data.quotes[symbol].bs
+                    const askSize = data.quotes[symbol].as
+                    await Publisher.set('bid:alpaca:'+symbol, bid)
+                    await Publisher.set('ask:alpaca:'+symbol, ask)
+                })
+            })
+            res.on('error', (err) => {
+                console.log(err)
             })
         })
+}
+
+export async function getStockPrices(symbols: string|string[]) {
+    if (!Array.isArray(symbols)) symbols = [symbols]
+    const resp = await alpaca.getLatestTrades(symbols)
+    resp.forEach((trade: any) => {
+        Publisher.set('price:alpaca:'+trade.Symbol, trade.Price)
     })
 }
 
-async function subscribeCandles(market: string, timeframe: string, first?: boolean) {
-    try {
-       // let candles = await getCandles(market, timeframe, first ? 200 : 1)
-       console.log('candles')
-    } catch(e) {
-        setTimeout(()=>subscribeCandles(market, timeframe, false), 1000)
-    }
-}
-
-
-async function executeMessage(message: string[]) {
-    switch (message[0]) {
-        case 'sub-trades' : {
-            if (!Subscriptions.toString().includes(message.toString())) {
-                server?.send(JSON.stringify({
-                    action : 'subscribe',
-                    trades : [message[1].toUpperCase()]
-                }))
-                process.send && process.send(['alpaca', 'sub-trades', message[1].toUpperCase()])
-                Subscriptions.push(message) 
-                Publisher.set('exchanges:alpaca:tracked', JSON.stringify(Subscriptions))
-            }
-            break;
+export async function getCryptoPrices(data: string|string[]) {
+    const symbols: string = Array.isArray(data) ? data.join(',') : data
+    symbols.replace('/','%2F')
+    const opts = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            'APCA-API-KEY-ID': process.env.ALPACA_KEY,
+            'APCA-API-SECRET-KEY': process.env.ALPACA_SECRET
         }
-        case 'unsub-trades' : {
-            if (Subscriptions.filter((sub) => { return (sub[0] == 'sub-trades' && sub[1] == message[1])}).length == 1) {
-                server?.send(JSON.stringify({
-                    action : 'unsubscribe',
-                    trades : [message[1].toUpperCase()],
-                }))
-                
-                Publisher.del(`${message[1].toUpperCase()}-alpaca`)
-                Subscriptions.forEach((sub, index)=>{
-                    if (sub[0] == 'sub-trades' && sub[1] == message[1]) {
-                        Subscriptions.splice(index,1)
-                        Publisher.set('exchanges:alpaca:tracked', JSON.stringify(Subscriptions))
-                    }
-                })
-            }
-            break;
-        }
-    }
-}
-
-
-process.on('message', (message: string[]) => {
-    executeMessage(message)
-})
-
-
-
-const equities = alpaca.data_stream_v2;
-const crypto = alpaca.crypto_stream_v2;
-const news = alpaca.news_stream;
-crypto.onConnect(async () => {
-    console.log('crypto connected')
-})
-crypto.onCryptoTrade((trade) => {
-    console.log('Crypto Trade:', trade);
-})
-crypto.onError((err) => {
-    console.log("Crypto Error:", err);
-})
-equities.onConnect(async () => {
-    process.send && process.send(['alpaca', 'open'])
-    console.log('open')
-});
-equities.onStateChange(async (status) => { 
-  console.log("Status:", status);
-  
-  switch (status) {
-    case 'authenticated' : {
-        await Publisher.connect()
-        const result = await Publisher.get('exchanges:alpaca:tracked') 
-        if (result) {
-            let trades: string[] = [], candles: string[] = []
-            JSON.parse(result).forEach( (subscription: string[]) => { 
-                subscription[0] == 'sub-trades' && trades.push(subscription[1])
-                subscription[0] == 'sub-candles' && candles.push(subscription[1])
+    };
+    https.get(`https://data.alpaca.markets/v1beta3/crypto/us/latest/trades?symbols=${symbols}`, opts, (res) => {
+        let data: any = ''
+        res.on('data', (chunk) => {
+            data += chunk
+        })
+        res.on('end', () => {
+            data = JSON.parse(data)
+            Object.keys(data.trades).forEach((symbol: string) => {
+                const trade = data.trades[symbol]
+                if (Date.now() - (new Date(trade.t)).getTime() > (60000*60)) return
+                Publisher.set('price:alpaca:'+symbol, trade.p)
             })
-            equities.subscribeForTrades(trades)
-            equities.subscribeForBars(candles)
-        }
-        break;
-    }
-  }
-});
-equities.onError((err) => {
-  console.log("Error:", err);
-});
+        })
+        res.on('error', (err) => {
+            console.log(err)
+        })
+    
+    })
 
-equities.onStockTrade((trade) => {
-  console.log(`${trade.Size} shares ${trade.Symbol} @ ${trade.Price}`)
-});
-
-equities.onStockBar((bar) => {
-    console.log("Bar:", bar)
-});
-
-equities.onStatuses((statuses) => {
-    console.log("Statuses:", statuses);
-})
-equities.addListener('message', (msg)=>{
-    console.log(msg)
-})
-equities.connect();
+}
